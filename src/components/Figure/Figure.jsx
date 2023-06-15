e8662f1b5a96433efbab78f856ad6d9647fa27ac
Jeff Manke <jeffman879@gmail.com>
2023-06-14 21:45:21 -0700

    apply max-width/height to figure (#33)
    * apply max-width/height to figure
    
    * left aligned figures

--- src/components/Figure/Figure.jsx
+++ src/components/Figure/Figure.jsx
@@ -11,20 +11,22 @@ export default function Footer(props) {
   return (
     <div className="figure">
       <img src={props.img} width="100%" height="100%" />
-      <h5>{props.title}</h5>
-      {props.credit !== "" && (
-        <div className="figure-credit">
-          <a
-            href={props.url}
-            target="_blank"
-            style={{
-              fontSize: "var(--dash-font-size-1)",
-            }}
-          >
-            {props.credit}
-          </a>
-        </div>
-      )}
+      <div className="figure-footer">
+        <h5>{props.title}</h5>
+        {props.credit !== "" && (
+          <div className="figure-credit">
+            <a
+              href={props.url}
+              target="_blank"
+              style={{
+                fontSize: "var(--dash-font-size-1)",
+              }}
+            >
+              {props.credit}
+            </a>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
