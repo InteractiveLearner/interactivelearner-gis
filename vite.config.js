9656f6aec119082a71d5ecb8ee2a89b486e66768
Omar Kawach <omarkawach@outlook.com>
2024-03-05 01:18:20 -0800

    update deps

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import mdx from "@mdx-js/rollup"
+import mdx from "@mdx-js/rollup";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { viteStaticCopy } from "vite-plugin-static-copy";
