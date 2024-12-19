7c0670fb9432e6f0ad630900326a105c884c9293
Sam Goyal <samarthya.goyal@jostle.me>
2024-12-19 01:18:28 -0800

    removed the unnecessary imports

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -15,7 +15,6 @@ import ArcgisCalciteMap from "../../components/mapping/ArcgisCalciteMap.vue";
 import ContentFigure from "../../components/ui/ContentFigure.vue";
 import Sources from "../../components/ui/Sources.vue";
 import Quiz from "../../components/ui/Quiz.vue";
-import HomePageGlobe from "../../components/mapping/HomePageGlobe.vue";
 
 // Layout
 import MyLayout from "./MyLayout.vue";
@@ -35,6 +34,5 @@ export default {
     app.component("ContentFigure", ContentFigure);
     app.component("Quiz", Quiz);
     app.component("Sources", Sources);
-    app.component("HomePageGlobe", HomePageGlobe)
   },
 };
